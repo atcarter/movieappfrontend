42f93ac68c240317122001a5f4ac2fd4dd81def5
Antonio <atcarter005@gmail.com>
2021-10-26 16:11:03 -0500

    Create review class

---
+++ src/review.js
@@ -0,0 +1,8 @@
+class Review {
+  constructor({content, rating, username, movie_id}){
+   this.content = content
+   this.rating = rating
+   this.username = username
+   this.movie_id = movie_id
+  }
+}
