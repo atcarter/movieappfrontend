32dc8937904482a25c40afdf9afe0b0e625749fe
Antonio <atcarter005@gmail.com>
2021-10-26 16:14:19 -0500

    Add id attribute to review class

--- src/review.js
+++ src/review.js
@@ -1,8 +1,9 @@
 class Review {
-  constructor({content, rating, username, movie_id}){
+  constructor({content, rating, username, id, movie_id}){
    this.content = content
    this.rating = rating
    this.username = username
+   this.id = id
    this.movie_id = movie_id
   }
 }
