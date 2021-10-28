f96004da37fef13c622c812a39e03f92ca276401
Antonio <atcarter005@gmail.com>
2021-10-28 07:45:17 -0500

    Add event listeners for both forms

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const movieCall = new MovieApi(port);
 
 const reviewForm = document.getElementById('review-form');
 const selectMovie = document.getElementById('select-movie');
+reviewForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+});
+
 const movieForm = document.getElementById('movie-form');
+movieForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+});
+
+
 
 
 reviewCall.getReviews();
