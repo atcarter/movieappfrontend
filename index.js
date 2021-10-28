dfaadd4687c49fb033da4d192e199a6ac1cec4da
Antonio <atcarter005@gmail.com>
2021-10-28 07:25:47 -0500

    Add variables for the two forms and the select tag

--- index.js
+++ index.js
@@ -2,5 +2,10 @@ const port = `http://localhost:3000`;
 const reviewCall = new ReviewApi(port);
 const movieCall = new MovieApi(port);
 
+const reviewForm = document.getElementById('review-form');
+const selectMovie = document.getElementById('select-movie');
+const movieForm = document.getElementById('movie-form');
+
+
 reviewCall.getReviews();
 movieCall.getMovies();
