7d861c8f2b5c039aa82ebc36a0909a1822a65e15
Antonio <atcarter005@gmail.com>
2021-10-28 08:23:00 -0500

    create variables for the inputs of the review and movie forms

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const reviewCall = new ReviewApi(port);
 const movieCall = new MovieApi(port);
 
 const reviewForm = document.getElementById('review-form');
+const reviewUser = document.getElementById('username');
+const reviewRating = document.getElementById('rating');
+const reviewContent = document.getElementById('content');
 const selectMovie = document.getElementById('select-movie');
+
 reviewForm.addEventListener('submit', function(event) {
   event.preventDefault();
 
@@ -11,6 +15,10 @@ reviewForm.addEventListener('submit', function(event) {
 });
 
 const movieForm = document.getElementById('movie-form');
+const movieTitle = document.getElementById('title');
+const movieYear = document.getElementById('year');
+const movieSynopsis = document.getElementById('synopsis');
+
 movieForm.addEventListener('submit', function(event) {
   event.preventDefault();
 
