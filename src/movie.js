441b6ee3abe46af27938e377e4d022a5502f4df2
Antonio <atcarter005@gmail.com>
2021-10-28 06:19:28 -0500

    Add element attribute to movie constructor. Add showMovie() method to READ movies

--- src/movie.js
+++ src/movie.js
@@ -1,13 +1,23 @@
 class Movie {
+  static all = [];
+
   constructor({id, title, year, synopsis, reviews}){
-    this.id = id
-    this.title = title
-    this.year = year
-    this.synopsis = synopsis
-    this.reviews = reviews
+    this.id = id;
+    this.title = title;
+    this.year = year;
+    this.synopsis = synopsis;
+    this.reviews = reviews;
+    this.element = document.createElement('li');
+    this.element.id = `movie-${this.id}`
+    Movie.all.push(this);
   }
 
-  renderMovie(){
-    
+  showMovie(){
+    this.element.innerHTML = `
+    <h2>${this.title} (${this.year})</h2>
+    <p>${this.synopsis}</p>
+    `
+    let ol = document.getElementById('movie-display-ul');
+    ol.appendChild(this.element);
   }
 }
