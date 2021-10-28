dbb83c4ee06d8228917d5d2c1f38da54776e1741
Antonio <atcarter005@gmail.com>
2021-10-28 05:22:16 -0500

    Add reviews attribute to movie object

--- src/movie.js
+++ src/movie.js
@@ -1,8 +1,9 @@
 class Movie {
-  constructor({id, title, year, synopsis}){
+  constructor({id, title, year, synopsis, reviews}){
     this.id = id
     this.title = title
     this.year = year
     this.synopsis = synopsis
+    this.reviews = reviews
   }
 }
