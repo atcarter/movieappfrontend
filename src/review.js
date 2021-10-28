b55ef158098dc1640f0dee10025dadbff6a08b5c
Antonio <atcarter005@gmail.com>
2021-10-28 07:16:20 -0500

    Update review class to match movie class. Add showReview() method

--- src/review.js
+++ src/review.js
@@ -1,14 +1,24 @@
 class Review {
+  static all = [];
+
   constructor({content, rating, username, id, movie_id}){
-   this.content = content
-   this.rating = rating
-   this.username = username
-   this.id = id
-   this.movie_id = movie_id
+   this.content = content;
+   this.rating = rating;
+   this.username = username;
+   this.id = id;
+   this.movie_id = movie_id;
+   this.element = document.createElement('li');
+   this.element.id = `review-${this.id}`
+   Review.all.push(this);
   }
   //need to remove the movies array from the reviews json
 
-  renderReview(){
-
+  showReview(){
+    this.element.innerHTML = `
+    <h4>${this.username} &emsp; &emsp; ${"*".repeat(this.rating)}</h4>
+    <p>${this.content}</p>
+    `
+    let ul = document.getElementById(`movie-${this.movie_id}-reviews`);
+    ul.appendChild(this.element);
   }
 }
