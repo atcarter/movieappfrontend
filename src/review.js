d791eb22e3cda027f415ef68b18aeb2a7521b37c
Antonio <atcarter005@gmail.com>
2021-10-29 23:27:13 -0500

    add event listener for deleting reviews

--- src/review.js
+++ src/review.js
@@ -9,13 +9,18 @@ class Review {
    this.movie_id = movie_id;
    this.element = document.createElement('li');
    this.element.id = `review-${this.id}`
+   this.element.addEventListener('click', function(event) {
+     if(event.target.innerText === "Delete"){
+       reviewCall.deleteReview(event);
+     }
+   })
    Review.all.push(this);
   }
   //need to remove the movies array from the reviews json
 
   showReview(){
     this.element.innerHTML = `
-    <h4>${this.username} &emsp; &emsp; ${"*".repeat(this.rating)}</h4>
+    <h4>${this.username} &emsp; &emsp; ${"*".repeat(this.rating)}&emsp;<button type=button id="review-${this.id}-delete">Delete</button></h4>
     <p>${this.content}</p>
     `
     let ul = document.getElementById(`movie-${this.movie_id}-reviews`);
